ce8d7bf58fb7e1801db83912c444629991a5b12b
Nguyễn Quang Thái <quangthai30062002@gmail.com>
2023-04-16 22:54:23 +0700

    Add new feature

---
+++ DIWebAPI/Dto/StudentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DIWebAPI.Dto
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string StudentCode { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+
+}
